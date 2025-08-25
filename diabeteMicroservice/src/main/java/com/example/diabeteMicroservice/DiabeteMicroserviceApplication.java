b596e6b8a8222603acf1c735699f104dbaad83d0
Emilien Beziat <emilien.beziat@gmail.com>
2025-08-25 16:12:56 +0200

    Ajout du microservice diabete (sprint 3)

---
+++ diabeteMicroservice/src/main/java/com/example/diabeteMicroservice/DiabeteMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.diabeteMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeteMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeteMicroserviceApplication.class, args);
+	}
+
+}
---
+++ diabeteMicroservice/src/test/java/com/example/diabeteMicroservice/DiabeteMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diabeteMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeteMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
